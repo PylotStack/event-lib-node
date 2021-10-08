3f46c31d17b8db6fa8def5c359f99cce9cfedf94
Jonah Nestrick <undecidedapollo@gmail.com>
2021-10-08 16:22:24 -0700

    Added more tests and improved typing for test utils

--- sample/bankAccount.test.ts
+++ sample/bankAccount.test.ts
@@ -2,7 +2,7 @@ import "jest";
 import { stack } from "../src/testUtils";
 import { bankAccount, bankAccountBalance } from "./bankAccount";
 
-it("should deposit 10 dollars",
+it("Should deposit 10, reject withdraw 15, commit withdraw 5",
     stack(bankAccount.definition)
 
         .onAction("DEPOSIT", { amount: 10 })
@@ -16,4 +16,66 @@ it("should deposit 10 dollars",
         .onAction("WITHDRAW", { amount: 5 })
         .commit()
         .assertView(bankAccountBalance.definition, { balance: 5 })
+);
+
+it("Should handle multiple deposits and withdrawls",
+    stack(bankAccount.definition)
+        .assertView(bankAccountBalance.definition, { balance: 0 })
+
+        .onAction("DEPOSIT", { amount: 5 })
+        .assertView(bankAccountBalance.definition, { balance: 5 })
+        .onAction("DEPOSIT", { amount: 5 })
+        .assertView(bankAccountBalance.definition, { balance: 10 })
+        .onAction("DEPOSIT", { amount: 5 })
+        .assertView(bankAccountBalance.definition, { balance: 15 })
+
+        .onAction("WITHDRAW", { amount: 5 })
+        .assertView(bankAccountBalance.definition, { balance: 10 })
+        .onAction("WITHDRAW", { amount: 5 })
+        .assertView(bankAccountBalance.definition, { balance: 5 })
+        .onAction("WITHDRAW", { amount: 5 })
+        .assertView(bankAccountBalance.definition, { balance: 0 })
+);
+
+it("Should fail on small withdrawl",
+    stack(bankAccount.definition)
+        .assertView(bankAccountBalance.definition, { balance: 0 })
+
+        .onAction("WITHDRAW", { amount: 0.01 })
+        .reject("ACCOUNT_BALANCE_LOW")
+        .assertView(bankAccountBalance.definition, { balance: 0 })
+);
+
+it("Should fail on negative withdrawl",
+    stack(bankAccount.definition)
+        .assertView(bankAccountBalance.definition, { balance: 0 })
+
+        .onAction("WITHDRAW", { amount: -0.01 })
+        .reject("AMOUNT_BELOW_ZERO")
+        .assertView(bankAccountBalance.definition, { balance: 0 })
+);
+
+it("Should handle suspensions",
+    stack(bankAccount.definition)
+        .onAction("DEPOSIT", { amount: 10 })
+        .assertView(bankAccountBalance.definition, { balance: 10 })
+        .onAction("SUSPEND", {suspended: true})
+
+        .onAction("WITHDRAW", { amount: 1 })
+        .reject("ACCOUNT_SUSPENDED")
+        .assertView(bankAccountBalance.definition, { balance: 10 })
+
+        .onAction("DEPOSIT", { amount: 1 })
+        .reject("ACCOUNT_SUSPENDED")
+        .assertView(bankAccountBalance.definition, { balance: 10 })
+
+        .onAction("SUSPEND", {suspended: false})
+
+        .onAction("WITHDRAW", { amount: 1 })
+        .commit()
+        .assertView(bankAccountBalance.definition, { balance: 9 })
+
+        .onAction("DEPOSIT", { amount: 1 })
+        .commit()
+        .assertView(bankAccountBalance.definition, { balance: 10 })
 );
--- src/testUtils.ts
+++ src/testUtils.ts
@@ -1,22 +1,21 @@
 import { localStack } from "./local";
 import { executeAction, compileView } from "./lib";
-import { ESEvent, EventStackDefinition, ViewDefinition, ActionHandlerEnum } from "./types";
+import { EventStackDefinition, ViewDefinition, ActionHandlerEnum } from "./types";
 import * as _ from "lodash";
-import { CloudFormation } from "aws-sdk";
 import * as assert from "assert";
 
-export interface BaseStackTestBuilder {
-    withEvents(events: Array<Record<string, any>>): StackTestBuilder;
-    onAction(actionType: string, payload: Record<string, any>): StackTestBuilder;
-    commit(): StackTestBuilder;
-    reject(errorType?: string): StackTestBuilder;
-    assertView(view: ViewDefinition, expectedModel: any): StackTestBuilder;
+export interface BaseStackTestBuilder<T extends string = null>  {
+    withEvents(events: Array<Record<string, any>>): StackTestBuilder<T>;
+    onAction(actionType: T, payload: Record<string, any>): StackTestBuilder<T>;
+    commit(): StackTestBuilder<T>;
+    reject(errorType?: string): StackTestBuilder<T>;
+    assertView(view: ViewDefinition, expectedModel: any): StackTestBuilder<T>;
     test(): Promise<void>;
 }
 
-export type StackTestBuilder = BaseStackTestBuilder & (() => Promise<void>);
+export type StackTestBuilder<T extends string = null> = BaseStackTestBuilder<T> & (() => Promise<void>);
 
-export function stack(stackDef: EventStackDefinition): StackTestBuilder {
+export function stack<T extends string = null>(stackDef: EventStackDefinition<T>): StackTestBuilder<T> {
     const self: StackTestBuilder = (() => self.test()) as unknown as StackTestBuilder;
 
     let definition = {
@@ -24,32 +23,41 @@ export function stack(stackDef: EventStackDefinition): StackTestBuilder {
         testCases: [],
     };
 
+    function createTestCase() {
+        return {
+            expectedViews: [],
+        };
+    }
+
+    function lastTestCase() {
+        return _.last(definition.testCases) ?? createTestCase();
+    }
+
     return Object.assign(self, {
         withEvents(events: Array<Record<string, any>>) {
             definition.baseEvents.push(...events);
             return self;
         },
         onAction(actionType: string, payload: Record<string, any>) {
-            definition.testCases.push({
+            definition.testCases.push(Object.assign(createTestCase(), {
                 actionType,
                 payload,
-                expectedViews: [],
-            });
+            }));
             return self;
         },
         commit() {
-            const testCase = _.last(definition.testCases);
+            const testCase = lastTestCase();
             testCase.expectedActionResult = ActionHandlerEnum.COMMIT;
             return self;
         },
         reject(errorType?: string) {
-            const testCase = _.last(definition.testCases);
+            const testCase = lastTestCase();
             testCase.expectedActionResult = ActionHandlerEnum.REJECT;
             testCase.expectedActionResultMessage = errorType;
             return self;
         },
         assertView(view: ViewDefinition, expectedModel: any) {
-            const testCase = _.last(definition.testCases);
+            const testCase = lastTestCase();
             testCase.expectedViews.push({ definition: view, state: expectedModel });
             return self;
         },
@@ -59,14 +67,16 @@ export function stack(stackDef: EventStackDefinition): StackTestBuilder {
                 await stack.commitEvent(event);
             }
             for (let testCase of definition.testCases) {
-                const res = await executeAction(stack, stackDef.actions[testCase.actionType], testCase.payload).catch((err) => err);
-                if (testCase.expectedActionResult) {
-                    if (testCase.expectedActionResult === ActionHandlerEnum.COMMIT && res?.action) throw new Error(`Expected result to be ${ActionHandlerEnum.COMMIT} but instead received ${res?.action}`);
-                    if (testCase.expectedActionResult === ActionHandlerEnum.COMMIT && res) throw res;
-                    if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && !res) throw new Error(`Expected result to be ${ActionHandlerEnum.REJECT} but instead received ${ActionHandlerEnum.COMMIT}`);
-                    if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && !res.action) throw res;
-                    if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && res.action !== ActionHandlerEnum.REJECT) throw new Error(`Expected result to be ${ActionHandlerEnum.REJECT} but instead received ${res.action}`);
-                    if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && res.result !== testCase.expectedActionResultMessage) throw new Error(`Expected result to be ${testCase.expectedActionResultMessage} but instead received ${res.result}`);
+                if (testCase.actionType) {
+                    const res = await executeAction(stack, stackDef.actions[testCase.actionType], testCase.payload).catch((err) => err);
+                    if (testCase.expectedActionResult) {
+                        if (testCase.expectedActionResult === ActionHandlerEnum.COMMIT && res?.action) throw new Error(`Expected result to be ${ActionHandlerEnum.COMMIT} but instead received ${res?.action}`);
+                        if (testCase.expectedActionResult === ActionHandlerEnum.COMMIT && res) throw res;
+                        if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && !res) throw new Error(`Expected result to be ${ActionHandlerEnum.REJECT} but instead received ${ActionHandlerEnum.COMMIT}`);
+                        if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && !res.action) throw res;
+                        if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && res.action !== ActionHandlerEnum.REJECT) throw new Error(`Expected result to be ${ActionHandlerEnum.REJECT} but instead received ${res.action}`);
+                        if (testCase.expectedActionResult === ActionHandlerEnum.REJECT && res.result !== testCase.expectedActionResultMessage) throw new Error(`Expected result to be ${testCase.expectedActionResultMessage} but instead received ${res.result}`);
+                    }
                 }
 
                 for (let expectedView of testCase.expectedViews) {
